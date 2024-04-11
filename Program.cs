86b7efaeea879e7ad8b42ce0ceeb5558da0721ae
gbprg <duarte.gabrielgbd@gmail.com>
2024-04-11 14:40:11 -0300

    feat: desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Teste com a classe Iphone
+            Iphone iphone = new Iphone("123456789", "iPhone 12", "123456789012345", 128);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+
+            // Teste com a classe Nokia
+            Nokia nokia = new Nokia("987654321", "Nokia 3310", "987654321098765", 16);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("Snake");
+
+            Console.ReadLine();
+        }
+    }
+}
